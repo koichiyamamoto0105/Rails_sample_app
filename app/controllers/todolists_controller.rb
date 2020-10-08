8eb68f4d55dc9fc180e7cd2dad7797ad64028aee
KoichiYamamoto <ko1y5osaka@gmail.com>
2020-10-08 08:49:51 +0000

    [Finished 2-6]

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,8 +2,6 @@ class TodolistsController < ApplicationController
   def new
     @list = List.new
   end
-
-
   def create
     # １．データを新規等ロックするためのインスタンス作成
     list = List.new(list_params)
@@ -12,14 +10,23 @@ class TodolistsController < ApplicationController
     # ３．トップ画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
+  def index
+    @lists = List.all
+  end
 
-    def index
-      @lists = List.all
-    end
+  def show
+    @list = List.find(params[:id])
+  end
 
-    def show
-      @list = List.find(params[:id])
-    end
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
 
   private
   # ストロングパラメータ
